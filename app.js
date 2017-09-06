6a014a06a41ec594b183a624bc3529e8c6e28db0
laurajan99 <laurijona@gmail.com>
2017-09-06 21:10:01 +0300

    paskaita6nd

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('app is up..');
+
+setTimeout(fn, 2000)
+
+function fn(){
+    console.log('2s atidejimas')
+};
+
+//var text = document.querySelector('.text');
+//var i = 0;
+//setInterval(function(){
+//    console.log (i);
+//    i++;
+//    text.box.p.transition= '1s';
+//    if(i==2){
+//        i=1
+//    }
+//    text.box.p= `('p${i}')`;
+//},2000)
+//            
+            
+            
+            
